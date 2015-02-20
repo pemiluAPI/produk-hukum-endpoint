45683e80f6b75b49c59b930982711d5756b01b18
Hendy <hendy.yanuar@kiranatama.com>
2015-02-20 15:08:19 +0700

    Prepare table migrations

---
+++ app/database/migrations/2015_02_20_063411_create_regulations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegulationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('regulations', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('category_id')->unsigned();
+			$table->text('title');
+			$table->text('description');
+			$table->string('file_url');
+			$table->string('url');
+			$table->nullableTimestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('regulations');
+	}
+
+}
